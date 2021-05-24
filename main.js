4f630e84fe1305b6056c525a893ff0fc6e3e872b
unknown <tatianahryhorenko@gmail.com>
2021-05-24 15:48:57 +0300

    --- Auto-Git Commit ---

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-/*const menuLinks = document.querySelectorAll('menu__link' [data - goto]);
-if (menuLinks.length > 0) {
-    menuLinks.forEach(menuLinks => {
-        menuLink.eddEventListener("click", onMenuLinkClick);
-
-    });
-
-    function onMenuLinkClick(e) {
-        const menuLink = e.target;
-        if (menuLink.dataset.goto && document.querySelector(menuLink.data.goto)) {
-            const gotoBlock = document.querySelector(menuLink.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClient.Rect().top + pageXOffset - document.querySelector('header').offsetHeight;
-            window.scrollTo({
-                top: gotoBlockValue,
-                behavior: "smooth"
-            });
-            e.preventDefault();
-        }
-
-    }
-}*/
-
 function myFunction(x) {
     if (x.matches) { // If media query matches
         document.body.style.backgroundColor = "#EBFFED";
