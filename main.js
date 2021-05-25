bce0a8004e167a1fe6edc71540a91d261b64808f
unknown <tatianahryhorenko@gmail.com>
2021-05-25 04:46:48 +0300

    --- Auto-Git Commit ---

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-function myFunction(x) {
-    if (x.matches) { // If media query matches
-        document.body.style.backgroundColor = "#EBFFED";
-    } else {
-        document.body.style.backgroundColor = "#FEFEFE";
-    }
-}
-
 function myFunction() {
-    var x = document.getElementById("myTopnav");
+    let x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
