934ef100fdd1763f05f3a3670e3c863aa25aec74
unknown <tatianahryhorenko@gmail.com>
2021-05-25 03:46:08 +0300

    --- Auto-Git Commit ---

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ function myFunction(x) {
         document.body.style.backgroundColor = "#FEFEFE";
     }
 }
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
